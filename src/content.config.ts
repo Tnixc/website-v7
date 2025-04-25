60c3ae2ca3647b1966f08b2b2f00c38e96ba76e0
Tnixc <tnixxc@gmail.com>
2025-04-24 22:57:05 -0400

    organization

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const writing = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: './src/content', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
@@ -11,8 +11,7 @@ const blog = defineCollection({
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
   }),
 });
 
-export const collections = { blog };
+export const collections = { writing };
