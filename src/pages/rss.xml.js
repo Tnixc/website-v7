395c6f52448294da8e8ba0b1542a00bdeaeb65e0
Tnixc <tnixxc@gmail.com>
2025-05-02 19:10:53 -0400

    update rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
+  const posts = await getCollection('writing');
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.id}/`,
+      link: `/writing/${post.id}/`,
     })),
   });
 }
